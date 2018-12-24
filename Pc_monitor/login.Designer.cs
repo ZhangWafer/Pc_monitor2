c26372af91809d08f644ba46c793065bbbd74d62
ZhangWafer <ZhangWafer@users.noreply.github.com>
2018-12-24 17:34:30 +0800

    Initial commit

---
+++ Pc_monitor/login.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Pc_monitor
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(640, 328);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(556, 322);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "账号";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(640, 382);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(210, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Pc_monitor.Properties.Resources.login_bg2;
+            this.button1.Font = new System.Drawing.Font("宋体", 30F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(561, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(289, 94);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(557, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "密码";
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Pc_monitor.Properties.Resources.login_bg2;
+            this.ClientSize = new System.Drawing.Size(1424, 862);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "login";
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Pc_monitor/login.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using Pc_monitor;
+using XinYu.Framework.Library.Factory;
+using XinYu.Framework.Library.Interface;
+
+namespace Pc_monitor
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string account = textBox1.Text;
+            string password = textBox2.Text;
+
+            var staticHashCryptorgrapher = CommonLibraryFactory.CreateInstance<IHashCryptographer>();
+            var hashedOldPwd = staticHashCryptorgrapher.CreateHash(password);
+            //hash加密
+
+            if (account.Length<=14)
+            {
+                DataTable accountTable = SqlHelper.ExecuteDataTable("select * from Cater.PCStaff where PCNum=" + account);
+                string tableData = accountTable.Rows[0][5].ToString();
+
+                if (hashedOldPwd == tableData )
+                {
+                    MessageBox.Show("登陆成功！");
+                }
+               
+            }
+            else 
+            {
+                DataTable accountTable2 = SqlHelper.ExecuteDataTable("select * from Cater.WorkerStaff where InformationNum=" + account);
+                if (hashedOldPwd == accountTable2.Rows[0][1])
+                {
+                    MessageBox.Show("登陆成功！");
+                }
+               
+            }
+
+        }
+
+
+
+    }
+
+}
